04b7e91d054484041d0507cce14a77dc6beadb05
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-04 23:27:24 +0200

    Implemented more logic to Data component

--- src/feature/MovieBrowser/Movies/List/PopularMovies/Data/index.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/index.js
@@ -1,26 +1,38 @@
 import { useSelector } from "react-redux"
 import MovieTile from "./MovieTile"
-import { Item, Wrapper } from "./styled"
+import { Item, StyledLink, Wrapper } from "./styled"
 import { selectGenres } from "../../movieListSlice"
 import { Header } from "../../../styled"
+import NoResults from "../../../../../../common/NoResults"
+import { useQueryParameter } from "../../../../../../core/QueryBox/useQueryParameter"
 
 const Data = ({ movies }) => {
-    
+
+    const query = useQueryParameter() || null;
     const genres = useSelector(selectGenres)
-    
+
     return (
         <>
-        <Header>Popular Movies</Header>
-        <Wrapper>
-            {movies.map((movie) =>
-                <Item key={movie.id}>
-                    <MovieTile
-                        movie={movie}
-                        genres={genres}
-                    />
-                </Item>
-            )}
-        </Wrapper >
+            {!movies.length ? (
+                <NoResults />
+            ) : (
+                <>
+                    <Header>{query ? `Search results for "${query}"` : "Popular movies"}</Header>
+                    <Wrapper>
+                        {movies.map((movie) =>
+                            <Item key={movie.id}>
+                                <StyledLink to={`/movies${movie.id}`}>
+                                    <MovieTile
+                                        movie={movie}
+                                        genres={genres}
+                                    />
+                                </StyledLink>
+                            </Item>
+                        )}
+                    </Wrapper >
+                </>
+            )
+            }
         </>
     )
 }
