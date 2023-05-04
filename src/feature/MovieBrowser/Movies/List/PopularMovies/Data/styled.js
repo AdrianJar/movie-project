91fc5a8cfffc77feb37900f34503c5d214252e83
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-04 23:27:33 +0200

    Added styled link to styled

--- src/feature/MovieBrowser/Movies/List/PopularMovies/Data/styled.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Wrapper = styled.section`
     display: grid;
@@ -15,4 +16,9 @@ export const Item = styled.div`
     @media (max-width: ${({theme})=>theme.breakpoints.mobileMax}px) {
         width: 100%;
     }
+`;
+
+export const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.black};
 `;
